e10ddb682abf8a054e98127c4b13c8b6bc339877
Alex <croitoriu.alexandru.code@google.com>
2026-06-18 02:02:05 +0300

    asd

--- src/application/apps.py
+++ src/application/apps.py
@@ -5,7 +5,7 @@ from pathlib import Path
 
 from django.apps import AppConfig
 
-
+ 
 class ApplicationConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "application"
